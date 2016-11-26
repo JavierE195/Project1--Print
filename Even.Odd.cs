000875cf71a20a106e7a0eb282e6a7b67691861a
JavierE195 <ellerbrockjavier@hotmail.com>
2016-11-25 23:17:45 -0600

    Add files via upload

---
+++ Even.Odd.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float Number1;
+            float Division;
+            float Remainder;
+            string line;
+
+            Console.Write("Insert number: ");
+            line = Console.ReadLine();
+            Number1 = int.Parse(line);
+            Division = Number1 / 2;
+            Remainder = Number1 % 2;
+           
+            if (Remainder > 0)
+            {
+                Console.Write("Is an odd number");
+            }
+            else
+            {
+                Console.Write("Is a even number");
+            }          
+            Console.ReadKey();
+
+
+             
+        }
+    }
+}
