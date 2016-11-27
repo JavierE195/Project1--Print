66bad6cbc2be0cb55f46d2e5f268ae12fa99af2e
JavierE195 <ellerbrockjavier@hotmail.com>
2016-11-26 18:59:47 -0600

    Add files via upload

---
+++ Biggest number.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biggest_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float Number1;
+            float Number2;
+            string line1;
+            string line2;
+
+            Console.Write("Insert the first number:  ");
+            line1 = Console.ReadLine();
+            Number1 = float.Parse(line1);
+
+            Console.Write("Insert the second number:  ");
+            line2 = Console.ReadLine();
+            Number2 = float.Parse(line2);
+
+            if (Number1 != Number2)
+            {
+                if (Number1 < Number2)
+                {
+                    Console.Write("The second number is greater");
+                }
+                else
+                {
+                    Console.Write("The first number is greater");
+                }
+            }
+            else
+            {
+                Console.Write("The numbers are equal");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
