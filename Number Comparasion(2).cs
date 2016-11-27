080e9105fec3adb6efbf86160f1b92f8799b47aa
JavierE195 <ellerbrockjavier@hotmail.com>
2016-11-26 20:34:50 -0600

    Add files via upload

---
+++ Number Comparasion(2).cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biggest_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float Number1;
+            float Number2;
+            string line1;
+            string line2;
+
+            Console.Write("Insert the first number:  ");
+            line1 = Console.ReadLine();
+            Number1 = float.Parse(line1);
+
+            Console.Write("Insert the second number:  ");
+            line2 = Console.ReadLine();
+            Number2 = float.Parse(line2);
+
+            if (Number1 != Number2)
+            {
+                if (Number1 < Number2)
+                {
+                    Console.Write("{0} is greater than {1}.", Number2, Number1);
+                }
+                else
+                {
+                    Console.Write("{0} is greater than {1}.", Number1, Number2);
+                }
+            }
+            else
+            {
+                Console.Write("The numbers are equal");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
